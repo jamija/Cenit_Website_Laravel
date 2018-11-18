10b7826499b38aad6f2e9e60e41df176c1d550d2
sherlockdison <dison.martinz@gmail.com>
2018-11-17 22:07:10 -0300

    migrations

---
+++ database/migrations/2018_11_17_204425_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('product_name')->unique();
+            $table->string('description');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->string('img1');
+            $table->string('img2');
+            $table->string('img3');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
